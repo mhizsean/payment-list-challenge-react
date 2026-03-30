8c3ae433273ce88599e1eab21acaaf559acc7ebc
Adeyi Esther <adeyiesther01@gmail.com>
2026-03-30 18:13:15 +0100

    set up

--- src/components/PaymentsPage.tsx
+++ src/components/PaymentsPage.tsx
@@ -1,6 +1,15 @@
 import React from "react";
-import { Container } from './components'
+import { Container } from "./components";
+import { I18N } from "../constants/i18n";
 
 export const PaymentsPage = () => {
-  return <Container>Start here</Container>;
+  return (
+    <Container>
+      <div className="px-6 py-4 border-b border-gray-200">
+        <h2 className="text-xl font-semibold text-gray-800">
+          {I18N.PAGE_TITLE}
+        </h2>
+      </div>
+    </Container>
+  );
 };
